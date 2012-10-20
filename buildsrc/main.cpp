59a6ffbc7a47958f80009d01490def6addebbabc
Dave Kilian <dave.kilian@gmail.com>
2012-10-19 20:52:06 -0400

    build-bakery can scan/dump plugin manifests

--- buildsrc/main.cpp
+++ buildsrc/main.cpp
@@ -1,13 +1,42 @@
 
 #include <iostream>
 
+#include "manifest.h"
 #include "scan.h"
+
+using namespace std;
 
 int main(int argc, const char *argv[]) 
 {
-    std::vector<std::string> cpps = scan("..", ".cpp");
-    for (size_t i = 0; i < cpps.size(); ++i)
-        std::cout << cpps[i] << "\n";
+    vector<string> manifests = scan("plugins", "BakeryPlugin");
+    for (size_t i = 0; i < manifests.size(); ++i)
+    {
+        PluginManifest pm;
+        if (!pm.load(manifests[i]))
+        {
+            cout << "Warning: couldn't parse " << manifests[i] << "\n";
+            continue;
+        }
+
+        cout << "Loaded:  " << manifests[i] << "\n"
+             << "Plugin:  " << pm.pluginName() << "\n"
+             << "By:      " << pm.orgName() << "\n"
+             << "Version: " << pm.pluginVersion() << "\n"
+             << "Include: " << pm.includes() << "\n"
+             << "Libs:    " << pm.libraries() << "\n"
+             << "Exports: \n";
+
+        const vector<PluginManifestObject> obj = pm.objects();
+        for (size_t j = 0; j < obj.size(); ++j)
+        {
+            const PluginManifestObject &o = obj[j];
+
+            cout << "         " << o.name << " : public " << objTypeToStr(o.type)
+                 << " [ " << o.header << " ] \n";
+        }
+
+        cout << "\n";
+    }
 
     return 0;
 }
