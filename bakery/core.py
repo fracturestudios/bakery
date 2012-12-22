2ff1bd604aea88fc4d6217784d77b0d5a8452784
Dave Kilian <dave.kilian@gmail.com>
2012-12-22 12:22:11 -0500

    Core module

--- bakery/core.py
+++ bakery/core.py
@@ -1,4 +1,5 @@
-""" Core API """
+
+import bakery.asset
 
 import glob
 import os
@@ -14,11 +15,7 @@ processors = { }    # processor name => callback
 exporters = { }     # exporter name => callback
 
 def load_extensions():
-    """ 
-    Searches _EXTROOT for C extensions and loads them into the current
-    interpreter. Returns a dictionary from the extension's module name to the
-    module itself.
-    """
+    """ Search for extensions in _EXTROOT and loads them """
 
     print "Loading extensions..."
     global extensions
@@ -48,17 +45,93 @@ def load_extensions():
             except:
                 print "error (not a module?)"
 
-def import_asset(TODO, arglist):
-    pass
+def all_importers():
+    """ Gets the name of every loaded importer extension """
+    return importers.keys()
 
-def process_asset(TODO, arglist):
-    pass
+def all_processors():
+    """ Gets the name of every loaded processor extension """
+    return processors.keys()
 
-def export_asset(TODO, arglist):
-    pass
+def all_exporters():
+    """ Gets the name of every loaded exporter extension """
+    return exporters.keys()
+
+def default_importer(path):
+    """ Gets the name of the default importer for the given file path """
+
+    ext = os.path.splitext(path)[1]
+    ext = ext[1:] # remove leading '.'
+
+    global importers
+    for name in importers:
+        exts = importers[name][0]
+        if ext in exts:
+            return name
+
+    return None
+
+def importer(name):
+    """ Gets the callback for the importer with the given name """
+    return importers[name][1]
+
+def processor(name):
+    """ Gets the callback for the processor with the given name """
+    return processors[name]
+
+def exporter(name):
+    """ Gets the callback for the exporter with the given name """
+    return exporters[name]
+
+def import_asset(stream, importer, args, asset):
+    """
+    Imports an asset by calling into an extension. If the importer fails for
+    some reason, an exception is thrown. No value is returned.
+
+    stream [file]   A file, opened in binary mode, with the seek pointer at the
+                    beginning. Contains the binary data that should be
+                    imported.
+    importer [str]  The name of the importer extension to use
+    args [dict]     A dictionary of string key/value pairs to pass to the
+                    importer
+    asset [Asset]   The bakery asset that will receive the loaded data.
+    """
+
+    cb = importers[importer][1]
+    cb(stream, args, asset)
+
+def process_asset(processor, args, asset):
+    """
+    Processes an asset by calling into an extension. If the processor fails for
+    some reason, an exception is thrown. No value is returned.
+
+    processor [str] The name of the processor extension to use
+    args [dict]     A dictionary of string key/value pairs to pass to the
+                    processor
+    asset [Asset]   The bakery asset containing the data to process. Processed
+                    data will be stored in the same asset.
+    """
+
+    cb = processors[processor]
+    cb(args, asset)
+
+def export_asset(stream, exporter, args, asset):
+    """
+    Exports an asset by calling into an extension. If the exporter fails for
+    some reason, an exception is thrown. No value is returned.
+
+    stream [file]   A file, opened in binary mode, with the seek pointer at the
+                    beginning. Receives the exported binary data.
+    exporter [str]  The name of the exporter extension to use
+    args [dict]     A dictionary of string key/value pairs to pass to the
+                    exporter
+    asset [Asset]   The bakery asset containing the data to export
+    """
+
+    cb = exporters[exporter]
+    cb(stream, args, asset)
 
 # TODO
-# - core module
 # - buildchain module
 # - bakefile module
 # - C99 helper library for extensions
